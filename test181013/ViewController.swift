858bd89b7f391507b59f3b43daa49991907cedef
xxxThinking <973986153@qq.com>
2018-12-30 01:41:56 +0800

    最终代码
    代码加注释

--- test181013/ViewController.swift
+++ test181013/ViewController.swift
@@ -37,17 +37,19 @@ public struct Stack<T> {
     }
 }
 
+//思想：
+//栈中寄存加减和get得到的乘除法计算得的值 乘除由后面的符号利用peek（）偷看栈顶的到并计算进栈
 
 class ViewController: UIViewController {
 
-    var temp:Double = 0
-    var remember: Bool = true
+    var temp:Double = 0  //屏幕输入的数字
+    var remember: Bool = true //小数点
     var count = 0 //符号标示
-    var temp1: Int = 0
     var count3 = 0//记录乘除前加减号
-    var sum : Double = 1
-    var count1 = 0  //记录第一个数字
-    var count2 = 0
+    var get : Double = 1 //栈外计算
+    var countFrist = 0  //记录第一个数字
+    var countNegative = 0  //记录乘除加法前是否有减号
+    var countget = 0 //记录 = 号调用次数
     @IBOutlet weak var display: UITextField!
     @IBAction func H1(_ sender: Any) {
         if(display.text == "0")
@@ -125,38 +127,42 @@ class ViewController: UIViewController {
             
         } else
         {
-        count = 1
+            count = 1
             temp = Double(display.text!)!
-            if(OmyStack.peek() == 2 ){
+            if(OmyStack.peek() == 2 ){//判断乘除加法前是否有减号 有则置为负数 进栈
                 temp *= -1
                 myStack.push(element: temp)
-                count2 += 1
+                countNegative += 1
             }else{
                 myStack.push(element: temp)
             }
             
-            if(count1 == 0){
+            if(countFrist == 0){  //判断是否为开头输入的元素
                 OmyStack.push(element: count)
             }else{
                 let x = OmyStack.peek()! //偷看前面是什么符号
             if(x == 3 || x == 4) //如果是乘除的话将乘除符号出栈
             {
-                let _ = OmyStack.pop()!
+                let _ = OmyStack.pop()!  //该符号前面为乘除法运算则f符号出栈
             }
                 switch(x){
-                    case 3: sum = myStack.pop()! / myStack.pop()!
-                    case 4: sum = myStack.pop()! * myStack.pop()!
-                    default : count3 += 1
+                    case 3: get = myStack.pop()! / myStack.pop()!
+                    case 4: get = myStack.pop()! * myStack.pop()!
+                    default : count3 += 1  //判断是加减号
                     }
-                if(x == 3||x == 4){
-                myStack.push(element: sum)
-                    sum = 1
+                if(x == 3||x == 4){  //乘除运算的值进栈
+                    if(x == 3) //判断为除号需要置换get为倒数
+                    {
+                        get = 1/get
+                    }
+                    myStack.push(element: get)
+                    get = 1 //进栈后重置总值
                 }
                 OmyStack.push(element: count)
             }
-            remember = true
-            display.text = ""
-            count1 += 1
+            remember = true //记录小数点是否输入
+            display.text = ""  //清空屏幕
+            countFrist += 1 //记录输入次数 便于查看是否为第一次输入
         }
     }
     @IBAction func reduce(_ sender: Any) {
@@ -165,33 +171,36 @@ class ViewController: UIViewController {
             
         } else
         {
-        count = 2
+            count = 2
             temp = Double(display.text!)!
             myStack.push(element:temp)
-            if(count1 == 0){
+            if(countFrist == 0){
                 OmyStack.push(element: count)
                 
             }else{
-                print(myStack.peek()!)
                 let x = OmyStack.peek()!
             if(x == 3 || x == 4) //如果是乘除的话将乘除符号出栈
             {
                 let _ = OmyStack.pop()!
             }
                 switch(x){
-                    case 3: sum = myStack.pop()! / myStack.pop()!
-                    case 4: sum = myStack.pop()! * myStack.pop()!
+                    case 3: get = myStack.pop()! / myStack.pop()!
+                    case 4: get = myStack.pop()! * myStack.pop()!
                     default : count3 += 1
                     }
                 if(x == 3||x == 4){
-                    myStack.push(element: sum)
-                    sum = 1
+                    if(x == 3)
+                    {
+                        get = 1/get
+                    }
+                    myStack.push(element: get)
+                    get = 1
                 }
-            OmyStack.push(element: count)
+                OmyStack.push(element: count)
             }
             remember = true
             display.text = ""
-            count1 += 1
+            countFrist += 1
         }
     }
     @IBAction func division(_ sender: Any) {
@@ -202,25 +211,36 @@ class ViewController: UIViewController {
         {
             count = 3
             temp = Double(display.text!)!
-            myStack.push(element:temp)
-            if(count1 == 0){
-                sum = myStack.pop()!
-                myStack.push(element: sum)
+            if(OmyStack.peek() == 2 ){  //判断乘除加法前是否有减号 有则置为负数 进栈
+                temp *= -1
+                myStack.push(element: temp)
+                countNegative += 1
+            }else{
+                myStack.push(element: temp)
+            }
+            if(countFrist == 0){
+                get = myStack.pop()!
+                myStack.push(element: get)
+                get = 1
             }else{
                 let x = OmyStack.peek()!
                 switch(x){
-                case 3: sum = myStack.pop()! / myStack.pop()!
-                case 4: sum = myStack.pop()! * myStack.pop()!
+                case 3: get = myStack.pop()! / myStack.pop()!
+                case 4: get = myStack.pop()! * myStack.pop()!
                 default : count3 += 1
                 }
                 if(x == 3||x == 4){
-                    myStack.push(element: sum)
+                    if(x == 3)
+                    {
+                        get = 1/get
+                    }
+                    myStack.push(element: get)
                 }
             }
             OmyStack.push(element: count)
             remember = true
             display.text = ""
-            count1 += 1
+            countFrist += 1
         }
     }
     @IBAction func multiplication(_ sender: Any) {
@@ -231,18 +251,18 @@ class ViewController: UIViewController {
         {
             count = 4
             temp = Double(display.text!)!
-            if(OmyStack.peek() == 2 ){
+            if(OmyStack.peek() == 2 ){  //判断乘除加法前是否有减号 有则置为负数 进栈
                 temp *= -1
                 myStack.push(element: temp)
-                count2 += 1
+                countNegative += 1
             }else{
                 myStack.push(element: temp)
             }
             
-            if(count1 == 0){
-                sum = myStack.pop()!
-                myStack.push(element: sum)
-                sum = 1
+            if(countFrist == 0){
+                get = myStack.pop()!
+                myStack.push(element: get)
+                get = 1
             }else {
                 let x = OmyStack.peek()!
                 if(x == 3 || x == 4) //如果是乘除的话将乘除符号出栈
@@ -250,19 +270,24 @@ class ViewController: UIViewController {
                     let _ = OmyStack.pop()!
                 }
                 switch(x){
-                case 3: sum = myStack.pop()! / myStack.pop()!
-                case 4: sum = myStack.pop()! * myStack.pop()!
+                case 3: get = myStack.pop()! / myStack.pop()!
+                case 4: get = myStack.pop()! * myStack.pop()!
                 default : count3 += 1
                 }
                 if(x == 3||x == 4){
-                    myStack.push(element: sum)
-                    sum = 1
+                    if(x == 3)
+                    {
+                        get = 1/get
+                    }
+                    myStack.push(element: get)
+                    get = 1
+                    
                 }
             }
             OmyStack.push(element: count)
             remember = true
             display.text = ""
-            count1 += 1
+            countFrist += 1
     }
     }
     @IBAction func HZ(_ sender: Any) {
@@ -270,22 +295,41 @@ class ViewController: UIViewController {
         {
         } else
         {
+            if(countget == 0){//判断等号输入次数做"="前计算
             temp = Double(display.text!)!
             if(OmyStack.peek() == 2 ){
                 temp *= -1
                 myStack.push(element: temp)
-                count2 += 1
+                countNegative += 1
             }else{
                 myStack.push(element: temp)
             }
             for _ in 1...OmyStack.count{
                 Calculation()
             }
-            sum = myStack.pop()!
-            count1 = 0
-            display.text = "\(sum)"
+            }else //否则做等号后计算
+            {
+                temp = Double(display.text!)!
+                if(OmyStack.peek() == 2 ){
+                    temp *= -1
+                    myStack.push(element: temp)
+                    countNegative += 1
+                }else{
+                    myStack.push(element: temp)
+                }
+                for _ in 1...OmyStack.count{
+                    Calculation()
+            }
+            }
+            get = myStack.pop()! //得到get出栈
+            display.text = "\(get)" //显示
+            myStack.push(element: get) //再入栈以做“=”后计算
+            countFrist = 0 //清零首数字计数
+            print(get)//显示
+            countget += 1 //记录等号使用次数
         }
 }
+    //正负号转换
     @IBAction func negative(_ sender: Any) {
         if(display.text == "0" || display.text == "")
         {
@@ -297,67 +341,72 @@ class ViewController: UIViewController {
                 display.text = ""
                return
             }
-            if(remember)
-            {
-                temp1 = Int(display.text!)! * -1
-                display.text = "\(temp1)"
-            }
-            else
-            {
                 temp = Double(display.text!)! * -1
                 display.text = "\(temp)"
-            }
         }
     }
+    //小数点
     @IBAction func Hdot(_ sender: Any) {
         if(display.text == "")
         {
             remember = false
         }else
         {
+            if(countget > 0){  //如果"="号调用次数 > 0 则不能输入小数点
+                remember = false
+            }else{
             if(remember){
                 display.text = display.text! + "."
+            }
             }
             remember = false
         }
     }
+    
+    //计算函数
     func Calculation() -> () {
-        if(OmyStack.count != 1){
-        let x = OmyStack.pop()!
+        if(OmyStack.count != 1){ //已经得到同级运算和负数 所以最后一定做加法运算
+        let x = OmyStack.pop()! //减号有特殊性所以分开考虑
         switch(x){
-        case 1: sum = myStack.pop()! + myStack.pop()!
-        case 3: sum = myStack.pop()! / myStack.pop()!
-        case 4: sum = myStack.pop()! * myStack.pop()!
-        default : count1 += 1
+        case 1: get = myStack.pop()! + myStack.pop()!
+        case 3: get = myStack.pop()! / myStack.pop()!
+        case 4: get = myStack.pop()! * myStack.pop()!
+        default : countFrist += 1  //无用变量使用当替罪羊
     }
         if(x == 2)
         {
-            if(count2 == 0){
-                sum = myStack.pop()! - myStack.pop()! //当没有负数时
-                print(5)
+            if(countNegative == 0){
+                get = myStack.pop()! - myStack.pop()! //当没有负数时
             }else{
-                sum = myStack.pop()! + myStack.pop()! //有负数时计算
+                get = myStack.pop()! + myStack.pop()! //有负数时计算
             }
         }
             if(x == 3)
             {
-                sum = 1/sum
+                get = 1/get
             }
         } else{
-            let _ = OmyStack.pop()!
-            sum = myStack.pop()! + myStack.pop()!
+            let y = OmyStack.pop()!
+                switch(y){
+                case 3: get = myStack.pop()! / myStack.pop()! //等号后计算使用
+                case 4: get = myStack.pop()! * myStack.pop()!  //等号后计算使用
+                default : get = myStack.pop()! + myStack.pop()!  //加法运算
+                }
+            if(y == 3)
+            {
+                get = 1/get
+            }
         }
-        print(sum)
-    myStack.push(element: sum)
+        myStack.push(element: get)  //计算一次入一次栈
     }
     
     @IBAction func HAC(_ sender: Any) {
-        display.text = ""
-        remember = true
-        sum = 0
-        count1 = 0
-        count2 = 0
-        count3 = 0
+        display.text = ""  //清空屏幕
+        remember = true //重置可以输入小数点次数
+        get = 0 //清零总值
+        countFrist = 0 //清零判断首数字
+        countNegative = 0 //清零式子中负数
+        count3 = 0 //清零记录乘除前加减号
     }
     override func viewDidLoad() {
         super.viewDidLoad()
